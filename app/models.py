c2c58c54b98c7ef5e9945cc381b49b4712e445b4
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-29 12:29:41 +0300

    add neighbourhood class to models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Neighbourhood(models.Model):
+      name = models.CharField(max_length=80)
+      location = models.CharField(max_length=250)
+      # resident = models.ForeignKey('User', on_delete=models.CASCADE)
+      census = models.PositiveIntegerField(default=0)
+      contacts = models.CharField(max_length=250)
+      image = models.ImageField(upload_to = 'hood/', default='scene.jpg')
+      def __str__(self):
+            return f'Neighbourhood {self.name}'  
