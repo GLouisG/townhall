2c340c88f03932945000aed51130889bf06a3d85
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-30 21:13:40 +0300

    add url for changing business contacts

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from .import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
 
 urlpatterns=[
     url('^$',views.home,name='home'),
     url(r'^you/', views.you, name="you"),
     url(r'^search/',  views, name='search'),
+    path("bizcontacts/<str:name>/",views.bizcontacts, name="bizcontacts"),    
 ]
